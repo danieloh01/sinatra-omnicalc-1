4e8363bb5799c41867753c86d87bb2e7f4d9fde5
danieloh <56809357+danieloh01@users.noreply.github.com>
2025-01-29 18:11:01 +0000

    new commits

--- app.rb
+++ app.rb
@@ -1,6 +1,38 @@
 require "sinatra"
 require "sinatra/reloader"
 
+get("/howdy") do
+  erb(:hello)
+end
+
+get("/goodbye") do
+  erb(:bye)
+end
+
+get("/square/new") do
+  erb(:new_square_calc)
+end
+
+get("/square/results") do
+  erb(:square_results)
+end
+
+get("/square_root/new") do
+  erb(:new_square_root_calc)
+end
+
+get("/square_root/results") do
+  erb(:square_root_results)
+end
+
+get("/payment/new") do
+  erb(:payment)
+end
+
+get("/payment/results") do
+  erb(:payment_results)
+end
+
 get("/") do
   "
   <h1>Welcome to your Sinatra App!</h1>
